0a8245c68fd0ba3504b33a28db5f73a24a82543e
Pierre <pierre.nygard@gmail.com>
2020-09-14 13:45:07 +0200

    first commit

---
+++ SpacePort/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SpacePort.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
