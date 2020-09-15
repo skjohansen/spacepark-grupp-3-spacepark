a4547d205ef23e55516b5568a5d692336fe08c66
Samir Ehsani <samirehsani19@gmail.com>
2020-09-15 10:54:18 +0200

    Created parkinglot and parkingspot models

---
+++ SpacePort/Models/Parkingspot.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpacePort.Models
+{
+    public class Parkingspot
+    {
+        [Key]
+        public int ParkingspotId { get; set; }
+        public int Size { get; set; }
+        public bool Occupied { get; set; }
+        public Parkinglot parkinglot { get; set; }
+    }
+}
