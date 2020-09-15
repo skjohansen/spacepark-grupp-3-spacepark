dc3f5aae931c7555bcbabc15a9395a3278c3af27
fjalling <jakob.fjalling@gmail.com>
2020-09-15 11:19:44 +0200

    Added Repository ParkingSpotRepository

---
+++ SpacePort/Services/Repositories/ParkingSpotRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace SpacePort.Services.Repositories
+{
+    public class ParkingSpotRepository
+    {
+        private readonly ILogger<ParkingSpotRepository> _logger;
+        public ParkingSpotRepository(ILogger<ParkingSpotRepository> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
