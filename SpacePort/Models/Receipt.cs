e5c3b24e891a8c32c1407dc98da44ea165833d3f
fjalling <jakob.fjalling@gmail.com>
2020-09-15 10:38:00 +0200

    @Fjalling Added Receipt Model

---
+++ SpacePort/Models/Receipt.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpacePort.Models
+{
+    public class Receipt
+    {
+        [Key] 
+        public int ReceiptId { get; set; }
+
+        public int Price { get; set; }
+        public DateTime RegistrationTime { get; set; }
+        public DateTime EndTime { get; set; }
+        
+        public ParkingSpot ParkingSpot { get; set; }
+    }
+}
