5821389a6edac08e6dd3464079aa86428de6f1dc
oskar morell <oskarmorell1998@hotmail.se>
2020-09-15 14:29:16 +0200

    @oskarmorell Added GetAll method for ParkinglotRepository

--- SpacePort/Services/Repositories/ParkinglotRepository.cs
+++ SpacePort/Services/Repositories/ParkinglotRepository.cs
@@ -1,13 +1,27 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using SpacePort.Models;
+using SpacePort.Services.Interfaces;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace PakingAPI.Services
 {
-    public class ParkinglotRepository
+    public class ParkinglotRepository : IParkinglotRepository
     {
+        private readonly DataContext _context;
         private readonly ILogger<ParkinglotRepository> _logger;
-        public ParkinglotRepository(ILogger<ParkinglotRepository> logger)
+        public ParkinglotRepository(DataContext context, ILogger<ParkinglotRepository> logger)
         {
+            _context = context;
             _logger = logger;
+        }
+
+        public virtual async Task<Parkinglot[]> GetAll()
+        {
+            _logger.LogInformation($"Getting all parkinglots");
+            IQueryable<Parkinglot> query = _context.Parkinglots;
+            return await query.ToArrayAsync();
         }
     }
 }
