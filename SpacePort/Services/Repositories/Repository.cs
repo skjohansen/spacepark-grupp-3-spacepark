2cdd03d6f5a19e38f8cb6b31ce91aaeb1287c7c9
Samir Ehsani <samirehsani19@gmail.com>
2020-09-15 14:24:51 +0200

    22- Created Repository for IRepository

---
+++ SpacePort/Services/Repositories/Repository.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.Logging;
+using SpacePort.Models;
+using SpacePort.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpacePort.Services.Repositories
+{
+    public class Repository : IRepository
+    {
+        private readonly DataContext _context;
+        private readonly ILogger<Repository> _logger;
+        public Repository(DataContext context, ILogger<Repository> logger)
+        {
+            _context = context;
+            _logger = logger;
+
+        }
+        public virtual void Add<T>(T entity) where T : class
+        {
+            _logger.LogInformation($"Adding entity of type {entity.GetType()}");
+            _context.Add(entity);
+        }
+
+        public virtual void Delete<T>(T entity) where T : class
+        {
+            _logger.LogInformation($"Deleting entity of type {entity.GetType()}");
+            _context.Remove(entity);
+        }
+
+        public async virtual Task<bool> Save()
+        {
+            _logger.LogInformation($"Saving changes");
+            return (await _context.SaveChangesAsync()) >= 0;
+        }
+
+        public virtual void Update<T>(T entity) where T : class
+        {
+            _logger.LogInformation($"Updating entity of type {entity.GetType()}");
+            _context.Update(entity);
+        }
+    }
+}
