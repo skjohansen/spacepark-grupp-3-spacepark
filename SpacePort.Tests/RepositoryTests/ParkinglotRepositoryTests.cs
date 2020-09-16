8e8cafc193b386e3e05cf4d44f559beb205be5f6
Pierre <pierre.nygard@gmail.com>
2020-09-16 13:59:14 +0200

    parkinglots repo test, förväntar få ut object, fungerar

--- SpacePort.Tests/RepositoryTests/ParkinglotRepositoryTests.cs
+++ SpacePort.Tests/RepositoryTests/ParkinglotRepositoryTests.cs
@@ -1,10 +1,34 @@
-﻿using System;
+﻿using Microsoft.Extensions.Logging;
+using Moq;
+using Moq.EntityFrameworkCore;
+using PakingAPI.Services;
+using SpacePort.Models;
 using System.Collections.Generic;
-using System.Text;
+using Xunit;
 
 namespace SpacePort.Tests.RepositoryTests
 {
-    class Class1
+    public class ParkinglotRepositoryTests
     {
+        [Fact]
+        public async void GetAllParkinglots_ReturnsObjects_ExpectedTrue()
+        {
+            // Arrange
+            var mockContext = new Mock<DataContext>();
+            mockContext.Setup(p => p.Parkinglots).ReturnsDbSet(new List<Parkinglot> 
+            {
+                new Parkinglot { ParkinglotId = 1, Name = "Coruscant" },
+                new Parkinglot { ParkinglotId = 2, Name = "Naboo" }
+            });
+
+            var logger = Mock.Of<ILogger<ParkinglotRepository>>();
+            var parkinglotRepository = new ParkinglotRepository(mockContext.Object, logger);
+
+            // Act
+            var result = await parkinglotRepository.GetAll();
+
+            // Assert
+            Assert.True(result.Length == 2);
+        }
     }
 }
