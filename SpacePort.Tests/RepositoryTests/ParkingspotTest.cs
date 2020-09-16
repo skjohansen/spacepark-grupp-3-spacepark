376a1a28c91f182497153eae41d9bf871b02c161
fjalling <jakob.fjalling@gmail.com>
2020-09-16 14:13:53 +0200

    Need to pull and merge master

---
+++ SpacePort.Tests/RepositoryTests/ParkingspotTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Moq;
+using SpacePort.Models;
+using Xunit;
+
+namespace SpacePort.Tests.RepositoryTests
+{
+    public class ParkingspotTest
+    {
+        [Fact]
+        public async void GettAllParkingspots()
+        {
+            //Arrange
+            var mockContext = new Mock<DataContext>();
+            mockContext.Setup(p => p.Parkingspots).ReturnsDbset(new List<Parkingspot>
+            {
+                new Parkingspot { ParkingspotId = 1, Name = ""}
+            })
+        }
+    }
+}
