015ffa4b7a98b9f16dbc26e625719423883f7cd3
oskar morell <oskarmorell1998@hotmail.se>
2020-09-17 11:21:53 +0200

    @oskarmorell Added CreateDriver method for Drivercontroller

--- SpacePort/Controllers/DriverController.cs
+++ SpacePort/Controllers/DriverController.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using SpacePort.Models;
 using SpacePort.Services.Interfaces;
 
@@ -12,10 +14,12 @@ namespace SpacePort.Controllers
     public class DriverController : Controller
     {
         private readonly IDriverRepository _repo;
+        private readonly HttpClient _client;
 
-        public DriverController(IDriverRepository repo)
+        public DriverController(IDriverRepository repo, HttpClient client)
         {
             _repo = repo;
+            _client = client;
         }
 
         [HttpGet]
@@ -33,6 +37,38 @@ namespace SpacePort.Controllers
             catch (Exception e)
             {
                 return StatusCode(StatusCodes.Status500InternalServerError, $"Database failure {e.Message}");
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> CreateDriver(Driver driver)
+        {
+            try
+            {
+                HttpResponseMessage response = await _client.GetAsync("https://swapi.dev/api/people/name/" + driver.Name);
+                if (response.IsSuccessStatusCode)
+                {
+                    Driver d1 = new Driver
+                    {
+                        DriverId = driver.DriverId,
+                        Name = driver.Name,
+                        Receipts = driver.Receipts
+                    };
+                    _repo.Add(d1);
+                    if(await _repo.Save())
+                    {
+                        return Created($"/api/v1.0/driver/{driver.DriverId}", d1);
+                    }
+                    return BadRequest();
+                }
+                else
+                {
+                    return StatusCode(StatusCodes.Status401Unauthorized, $"You are not part of Star Wars universe");
+                }
+            }
+            catch (Exception e)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, $"Database failure: {e.Message}");
             }
         }
     }
