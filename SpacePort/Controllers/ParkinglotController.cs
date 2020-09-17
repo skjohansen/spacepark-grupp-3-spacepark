4e74130d3ed826b2707510adb2a3aef0f8f0e4de
fjalling <jakob.fjalling@gmail.com>
2020-09-17 14:19:55 +0200

    Added GetById to parkinglotsController

--- SpacePort/Controllers/ParkinglotController.cs
+++ SpacePort/Controllers/ParkinglotController.cs
@@ -36,5 +36,27 @@ namespace SpacePort.Controllers
                 return this.StatusCode(StatusCodes.Status500InternalServerError, $"Database failure {e.Message}");
             }
         }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Parkinglot>> GetParkinglotById(int id)
+        {
+            try
+            {
+                var result = await _repo.GetParkinglotById(id);
+                if (result == null)
+                {
+                    return NotFound();
+                }
+                return Ok(result);
+            }
+            catch (Exception e)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, $"Database failure {e.Message}");
+
+            }
+        }
+
+
+
     }
 }
